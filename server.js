8461319593cee4bf19ab6e2bd7fa34655aa79191
Bryan-OF <brian.ortizfranco@hotmail.com>
2023-03-08 19:48:31 +0100

    Added fronted and made backend changes

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+// how does require work?
+// https://javascript.plainenglish.io/how-do-nodejs-require-and-module-work-545ab4fe5423
 const express = require('express');
+// https://expressjs.com/en/resources/middleware/cors.html
+const cors = require('cors');
+
 const app = express();
 const port = 8080;
 
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/profile', (req, res) => res.json({ name: 'Tim Damen' }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
