4b1f3675b485bd8809b659a0075191b9bc199411
Bryan-OF <brian.ortizfranco@hotmail.com>
2023-03-27 18:40:24 +0200

    added styling and

--- static/script/script.js
+++ static/script/script.js
@@ -1,8 +1,20 @@
-console.log('Hello World!');
+// Like functie
+function like(gameId, userId) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","/like/" + gameId + '/' + userId ,true);
+    xmlhttp.send();
+    // Code om een spel toe te voegen aan de favorieten van de gebruiker
+}
 
-// function like(gameId, userId)
-// {
-// var xmlhttp=new XMLHttpRequest();
-// xmlhttp.open("GET","/like/" + gameId + '/' + userId ,true);
-// xmlhttp.send();
-// } 
+
+// Unlike functie
+function unlike(likeId, userId) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET","/unlike/" + likeId + '/' + userId,true);
+    xmlhttp.send();
+
+    setTimeout(function(){ location.reload(); }, 1250);
+    // Code om een spel te verwijderen uit de favorieten van de gebruiker
+}
